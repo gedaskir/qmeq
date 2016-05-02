64610bf004a03d9595a5cf45c54dd317fde5e89d
Gediminas Kirsanskas <gediminas.kirsanskas@teorfys.lu.se>
2016-05-02 15:56:45 +0200

    Fixed neumann/setupc.py

--- neumann/setupc.py
+++ neumann/setupc.py
@@ -5,29 +5,27 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy as np
 
-if __name__ == "main":
+ext = [Extension("neumannc",
+                 ["neumannc.pyx"],
+                 #libraries = ["test"],
+                 #extra_compile_args = ["-O3"],
+                 extra_link_args=[#"./fortran/libgfortran.a"
+                                  "./fortran/digamma.o",
+                                  "./fortran/cpsi.o",
+                                  "./quadpack/dqc25c.o",
+                                  "./quadpack/dqcheb.o",
+                                  "./quadpack/dqk15w.o",
+                                  "./quadpack/dqwgtc.o",
+                                  "./quadpack/dqawc.o",
+                                  "./quadpack/dqawce.o",
+                                  "./quadpack/dqpsrt.o",
+                                  "./fortran/d1mach.o",
+                                  "./fortran/pvalint.o"
+                                  ]),
+      Extension("neumann2c",
+                ["neumann2c.pyx"])]
 
-    ext = [Extension("neumannc",
-                     ["neumannc.pyx"],
-                     #libraries = ["test"],
-                     #extra_compile_args = ["-O3"],
-                     extra_link_args=[#"./fortran/libgfortran.a"
-                                      "./fortran/digamma.o",
-                                      "./fortran/cpsi.o",
-                                      "./quadpack/dqc25c.o",
-                                      "./quadpack/dqcheb.o",
-                                      "./quadpack/dqk15w.o",
-                                      "./quadpack/dqwgtc.o",
-                                      "./quadpack/dqawc.o",
-                                      "./quadpack/dqawce.o",
-                                      "./quadpack/dqpsrt.o",
-                                      "./fortran/d1mach.o",
-                                      "./fortran/pvalint.o"
-                                      ]),
-          Extension("neumann2c",
-                    ["neumann2c.pyx"])]
+cext = cythonize(ext)
 
-    cext = cythonize(ext)
-
-    setup(include_dirs = [np.get_include()],
-          ext_modules = cext)
+setup(include_dirs = [np.get_include()],
+      ext_modules = cext)
