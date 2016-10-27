1514ac7f09ed954ad848e6e48f673720e2293be0
Gediminas Kirsanskas <gediminas.kirsanskas@teorfys.lu.se>
2016-10-27 23:26:19 +0200

    Changed the way cython extensions are treated in setup.py.

--- setup.py
+++ setup.py
@@ -2,31 +2,35 @@
 #from __future__ import division
 from __future__ import print_function
 from setuptools import setup
+from setuptools import Extension
+from Cython.Build import cythonize
+import numpy as np
 
-import os
-import sys
-import subprocess
+ext = [# Pauli, Redfield, 1vN approaches
+       Extension("qmeq.neumannc",
+                 ["qmeq/neumannc.pyx"],
+                 #libraries = ["test"],
+                 #extra_compile_args = ["-O3"],
+                 extra_link_args=[]),
+       # Lindblad approach
+       Extension("qmeq.lindbladc",
+                 ["qmeq/lindbladc.pyx"]),
+       # 2vN approach
+       Extension("qmeq.neumann2c",
+                 ["qmeq/neumann2c.pyx"])]
 
-# Save the command line arguments, because sys.argv will be modified by 'qmeq/setupc.py'
-argv = list(sys.argv)
-# Build Cython extensions
-cwd = os.path.abspath(os.path.dirname(__file__))
-print("Entering", os.path.join(cwd, 'qmeq'))
-os.chdir('qmeq')
-sys.path.append("./")
-import setupc
-print("Going back to", cwd)
-os.chdir('..')
-# Restore the initial command line arguments
-sys.argv = argv
+cext = cythonize(ext)
 
 setup(name='qmeq',
       version='0.0',
       description='Package for solving master equations',
-      #url='http://github.com/xxxx/qmeq',
+      url='http://github.com/gedaskir/qmeq',
       author='Gediminas Kirsanskas',
       author_email='gediminas.kirsanskas@teorfys.lu.se',
       license='MIT',
       packages=['qmeq'],
       package_data={'qmeq': ['*.pyx', '*.c', '*.pyd', '*.o', '*.so']},
-      zip_safe=False)
+      zip_safe=False,
+      install_requires=['numpy', 'scipy'],
+      include_dirs = [np.get_include()],
+      ext_modules = cext)
